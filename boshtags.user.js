0e36d3d2c05a4c55384c3b3fe9947ca4273f592f
tdwright <tom@tdwright.co.uk>
2012-05-19 14:51:07 +0100

    Initial commit - script works, but there is scope for extension. Release notes to follow.

---
+++ boshtags.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          BoshTags!
+// @namespace     http://tdwright.co.uk/userscripts
+// @description	  Detects boshtags and links them
+// @include       https://twitter.com/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+function linkBoshtags (jNode) {
+	if (jNode.attr('boshed')=='true') return;
+	var newContent = jNode.html().replace(/(\W)(\u273A(\w+))(\W)/g,"$1<a class='boshtag' href='https://twitter.com/#!/search/$2'>$2</a>$4");
+	jNode.html(newContent);
+	jNode.attr('boshed', 'true');
+}
+
+waitForKeyElements ("div.stream-container div.stream div.tweet p.js-tweet-text", linkBoshtags, false);
