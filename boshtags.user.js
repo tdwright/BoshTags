8e4180a3d19c625e29c122792b4009fdf636e115
Grant Thomas <g.thomas541@btinternet.com>
2012-05-28 21:13:37 +0100

    Made it work in Chrome. Really.
    This actually removes the jQuery import and relies on the existing
    'jQuery' reference. Seems to work reliably in Chrome and Firefox.

--- boshtags.user.js
+++ boshtags.user.js
@@ -7,15 +7,14 @@
 // @include       https://twitter.com/*
 // ==/UserScript==
 
-// a function that loads script from given sources and calls a callback function when finished loading
-// this may need to be tweaked for reliability but right now on an avg connection this works
+// a function that loads scripts from given sources and calls a callback function when finished loading the last
 // attr: from http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
 // which in turn is from http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
 function addScripts (sources, callback) {
   for (var i = 0; i < sources.length; i++) {
     var script = document.createElement("script");
     script.setAttribute("src", sources[i]);
-    if (i == sources.length - 1) {
+    if (i == sources.length - 1 && callback != null) {
       script.addEventListener('load', function() {
         var script = document.createElement("script");
         script.textContent = "(" + callback.toString() + ")();";
@@ -27,24 +26,23 @@ function addScripts (sources, callback) {
 }
 
 function prepareToBosh () {
-  $.noConflict();
-  
-  function linkBoshtags (jNode) {
-    if (jNode.attr('boshed')=='true') return;
-    var newContent = jNode.html().replace(/(\W|^)(\u273A(\w+))(?=\W|$)/g,"$1<a class='boshtag' href='https://twitter.com/#!/search/$2'>$2</a>");
-    jNode.html(newContent);
-    jNode.attr('boshed', 'true');
-  }
-
-  // For the stream
-  waitForKeyElements ("div.stream-container div.stream div.tweet p.js-tweet-text", linkBoshtags, false);
-  // For single tweet pages
-  waitForKeyElements ("div.permalink-tweet p.js-tweet-text", linkBoshtags, false);
+  jQuery(document).ready(function() {  
+    function linkBoshtags (jNode) {
+      if (jNode.attr('boshed')=='true') return;
+      var newContent = jNode.html().replace(/(\W|^)(\u273A(\w+))(?=\W|$)/g,"$1<a class='boshtag' href='https://twitter.com/#!/search/$2'>$2</a>");
+      jNode.html(newContent);
+      jNode.attr('boshed', 'true');
+    } 
+    // For the stream
+    waitForKeyElements ("div.stream-container div.stream div.tweet p.js-tweet-text", linkBoshtags, false);
+    // For single tweet pages
+    waitForKeyElements ("div.permalink-tweet p.js-tweet-text", linkBoshtags, false);
+  });
 }
 
 addScripts(
   [
-    "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js", 
+    //"https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js", 
     "https://gist.github.com/raw/2625891/waitForKeyElements.js"
   ],
   prepareToBosh);
